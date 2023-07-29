c8504f978ed55210edac0dc736dcad7a11e8071e
dxMausam7 <bajimayamausam@gmail.com>
2023-07-29 22:28:40 +0545

    Bonsai website added

---
+++ java script/script.js
@@ -0,0 +1,7 @@
+
+//Jave script code for changing the comments given by users in blogs page
+function change(){
+  var element = document.getElementById('set');
+  element.innerHTML = "Very useful Contain";
+  element.style.color = "Black";
+}
